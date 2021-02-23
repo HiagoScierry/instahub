772b9200d3478b8f87392871f2b0bf570de8f7d2
HiagoScierry <hiagocarlosmoreira@hotmail.com>
2021-02-22 23:00:43 -0300

    create route bar

---
+++ src/routes/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/Ionicons';
+
+import Friends from '../Pages/Friends';
+import Profile from '../Pages/Profile';
+import Search from '../Pages/Search';
+
+const Tab = createBottomTabNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Search"
+        tabBarOptions={{
+          activeTintColor: '#e91e63',
+          labelStyle: {
+            fontSize: 14,
+          },
+        }}>
+        <Tab.Screen
+          name="Friends"
+          component={Friends}
+          options={{
+            tabBarLabel: 'Amigos',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="heart" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarLabel: 'Pesquisa',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: 'Perfil',
+            tabBarIcon: ({color, size}) => (
+              <Icon2 name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
